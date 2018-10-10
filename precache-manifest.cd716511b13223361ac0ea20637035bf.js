079bb5c02e782908fd471e443fb7fa737215953f
Marshall Slemp <marshall.slemp@gmail.com>
2018-10-10 13:29:19 -0500

    Updates

---
+++ precache-manifest.cd716511b13223361ac0ea20637035bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5dc18037a973f0a98d2",
+    "url": "/static/js/main.e5dc1803.chunk.js"
+  },
+  {
+    "revision": "8bfb27a1cee6952d85ab",
+    "url": "/static/js/1.8bfb27a1.chunk.js"
+  },
+  {
+    "revision": "e5dc18037a973f0a98d2",
+    "url": "/static/css/main.930b9309.chunk.css"
+  },
+  {
+    "revision": "062821f1f5bc4aa40caa6c0d146f34cb",
+    "url": "/index.html"
+  }
+];
